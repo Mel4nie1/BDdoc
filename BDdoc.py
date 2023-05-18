d95148494811fba189e2356cf33d3487841a6ac8
Mel4nie1 <131460872+Mel4nie1@users.noreply.github.com>
2023-05-18 19:53:01 +0200

    Update BDdoc.py

--- BDdoc.py
+++ BDdoc.py
@@ -49,15 +49,6 @@ elif authentication_status == False:
 elif authentication_status == None:
     st.warning('Please enter your username and password')
     st.stop()
-# Setzen des Titels und Untertitels    
-st.set_page_config(
-    page_title="BDdoc - Überblick über deine Blutdruckwerte",
-    layout="wide",  # Ändern Sie dies entsprechend Ihren Layout-Anforderungen
-    initial_sidebar_state="expanded"  # Ändern Sie dies entsprechend Ihren Anforderungen
-)
-
-# Hintergrundbildfarbe auf Rot ändern
-st.markdown("<style>.stApp {background-color: #FFC0CB;}</style>", unsafe_allow_html=True)
 
 
 # Profilbild hochladen
