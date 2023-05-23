eed2ae8b8e37e7b47e45bee95c864f2bc399d1d0
Mel4nie1 <131460872+Mel4nie1@users.noreply.github.com>
2023-05-23 23:28:54 +0200

    Update jsonbin.py

--- jsonbin.py
+++ jsonbin.py
@@ -4,19 +4,19 @@ BIN_API_URL = r'https://api.jsonbin.io/v3/b'
 
 def load_data(api_key, bin_id):
     """
-    Load entire bin
+    Gesamte Bin laden
     """
-    url = BIN_API_URL + '/' + bin_id + '/latest'
+    url = f"{BIN_API_URL}/{bin_id}/latest"
     headers = {'X-Master-Key': api_key}
     res = requests.get(url, headers=headers).json()
     return res['record']
 
 
-def save_data(api_key, bin_id1, data):
+def save_data(api_key, bin_id, data):
     """
-    Save entire bin
+    Gesamte Bin speichern
     """
-    url = BIN_API_URL + '/' + bin_id
+    url = f"{BIN_API_URL}/{bin_id}"
     headers = {'X-Master-Key': api_key, 'Content-Type': 'application/json'}
     res = requests.put(url, headers=headers, json=data).json()
     return res
@@ -24,9 +24,9 @@ def save_data(api_key, bin_id1, data):
 
 def load_key(api_key, bin_id, key, empty_value=[]):
     """
-    Load key from bin
+    Schlüssel aus der Bin laden
     """
-    url = BIN_API_URL + '/' + bin_id + '/latest'
+    url = f"{BIN_API_URL}/{bin_id}/latest"
     headers = {'X-Master-Key': api_key}
     res = requests.get(url, headers=headers).json()
     res = res['record']
@@ -38,22 +38,23 @@ def load_key(api_key, bin_id, key, empty_value=[]):
 
 def save_key(api_key, bin_id, key, data):
     """
-    Save key to bin
+    Schlüssel in der Bin speichern
     """
-    url = BIN_API_URL + '/' + bin_id
+    url = f"{BIN_API_URL}/{bin_id}"
     headers = {'X-Master-Key': api_key, 'Content-Type': 'application/json'}
     res = requests.get(url, headers=headers).json()
     res = res['record']
     if type(res) != dict:
-        res = {key:data}  # generate new dict
+        res = {key: data}  # Neues Dictionary generieren
     else:
         res[key] = data
     res = requests.put(url, headers=headers, json=res).json()
     return res
 
+
 def load_notes(api_key):
     """
-    Load notes from bin
+    Notizen aus der Bin laden
     """
     data = load_data(api_key)
     return data.get('notizen', [])
@@ -61,7 +62,7 @@ def load_notes(api_key):
 
 def save_notes(api_key, notes):
     """
-    Save notes to bin
+    Notizen in der Bin speichern
     """
     data = load_data(api_key)
     data['notizen'] = notes
@@ -70,22 +71,8 @@ def save_notes(api_key, notes):
 
 def delete_notes(api_key):
     """
-    Delete notes from bin
+    Notizen aus der Bin löschen
     """
     return save_notes(api_key, [])
 
-def save_key(api_key, bin_id, username, data):
-    # Hier wird die Logik zur Speicherung der Daten in der JSONBin-Bin implementiert
-    # Du solltest die entsprechende Implementierung für den Zugriff auf die JSONBin-Bin und das Speichern der Daten verwenden
-
-    # Beispielhafte Implementierung:
-    try:
-        # Speichern der Daten in der JSONBin-Bin
-        # Hier erfolgt der entsprechende Code zur Speicherung der Daten
-
-        # Erfolgreiche Speicherung, "success" mit True zurückgeben
-        return {"success": True}
-    except Exception as e:
-        # Fehler bei der Speicherung, "success" mit False zurückgeben
-        return {"success": False, "error": str(e)}
 
