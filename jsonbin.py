4b3f1c57021e46314f69f07750928cd3aff777d0
Mel4nie1 <131460872+Mel4nie1@users.noreply.github.com>
2023-05-28 12:44:26 +0200

    Update jsonbin.py

--- jsonbin.py
+++ jsonbin.py
@@ -77,18 +77,18 @@ def delete_notes(api_key):
     return save_notes(api_key, [])
 
 
-# Funktion zum Speichern des Profils in der JSON-Bin
-def save_profile(api_key, bin_id, profile_data):
-
-    
-    def load_profile(api_key, bin_id):
+# Funktion zum Laden des Profils aus der JSON-Bin
+def load_profile(api_key, bin_id):
     url = f"https://api.jsonbin.io/v3/b/{bin_id}/latest"
     headers = {
         "X-Master-Key": api_key
     }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        # Handle error case
-        return None
+
+
+# Funktion zum Speichern des Profils in der JSON-Bin
+def save_profile(api_key, bin_id, profile_data):
+    url = f"https://api.jsonbin.io/v3/b/{bin_id}"
+    headers = {
+        "Content-Type": "application/json",
+        "X-Master-Key": api_key
+    }
