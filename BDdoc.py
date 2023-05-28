279786f684911c234e1a27b687735d9dafbd8898
Mel4nie1 <131460872+Mel4nie1@users.noreply.github.com>
2023-05-28 12:38:40 +0200

    Update BDdoc.py

--- BDdoc.py
+++ BDdoc.py
@@ -66,6 +66,28 @@ st.subheader("Überblick über deine Blutdruckwerte")
 st.markdown(""" <style>.stApp {background-color: #FFC0CB;}</style>""",
  unsafe_allow_html=True)
 
+# Laden des Profils aus der JSON-Bin
+profile_data = load_profile(api_key, bin_id1)
+
+# Profil-Eingabesektion
+st.subheader("Profilinformationen")
+fullname = st.text_input("Vollständiger Name", profile_data.get("fullname", ""))
+age = st.number_input("Alter", min_value=0, value=profile_data.get("age", 0))
+gender = st.selectbox("Geschlecht", ["Männlich", "Weiblich", "Andere"], index=profile_data.get("gender", 0))
+height = st.number_input("Größe (in cm)", min_value=0, value=profile_data.get("height", 0))
+weight = st.number_input("Gewicht (in kg)", min_value=0, value=profile_data.get("weight", 0))
+
+# Schaltfläche zum Speichern des Profils
+if st.button("Profil speichern"):
+    # Profil-Daten aktualisieren
+    profile_data["fullname"] = fullname
+    profile_data["age"] = age
+    profile_data["gender"] = gender
+    profile_data["height"] = height
+    profile_data["weight"] = weight
+
+    # Profil in der JSON-Bin speichern
+    save_profile(api_key, bin_id1, profile_data)
 
 
 # Dummy-Daten
